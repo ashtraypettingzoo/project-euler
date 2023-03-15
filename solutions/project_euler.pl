a9f3ac168e5dc744fe870e55bf36c8207e5e8ac8
cadence <christiancalhoun2.0@gmail.com>
2023-03-15 11:48:13 -0400

    updated prolog info

--- solutions/project_euler.pl
+++ solutions/project_euler.pl
@@ -1,5 +1,5 @@
-% SWI-Prolog:
-% online:
+% SWI-Prolog: https://www.swi-prolog.org/
+% online: https://swish.swi-prolog.org/
 
 p001(X) :- setof(Y, p001a(Y), Z), sumlist(Z, X).
 p001a(X) :- between(0, 999, X), (X mod 3 < 1; X mod 5 < 1).
