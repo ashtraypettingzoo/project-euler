6685f84b95f95bf20597fd238cc1e89bdf2bc7a0
cadence <christiancalhoun2.0@gmail.com>
2023-03-28 08:49:36 -0400

    added sols 22 24 25 29 in haskell

--- solutions/project_euler.hs
+++ solutions/project_euler.hs
@@ -2,11 +2,16 @@
 -- online: https://play.haskell.org/
 
 import Control.Applicative
+import Control.Arrow
+import Control.Monad
 import Data.Bool
+import Data.Char
 import Data.Function
 import Data.List
+import Data.Maybe
+import System.IO.Unsafe
 
-fibs :: [Int]
+fibs :: [Integer]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 primeFactors :: Int -> [Int]
@@ -35,8 +40,19 @@ fromDigits = foldr (\x y -> x + 10 * y) 0 . reverse
 triangluarNums :: [Int]
 triangluarNums = scanl1 (+) [1..]
 
-factors :: Int -> [Int]
-factors x = [y | y <- [1..x], 0 == mod x y]
+divisors :: Int -> [Int]
+divisors x = [y | y <- [1..x], 0 == mod x y]
+
+properDivisors :: Int -> [Int]
+properDivisors = safeInit . divisors
+    
+safeInit :: [a] -> [a]
+safeInit [] = []
+safeInit xs = init xs
+
+safeTail :: [a] -> [a]
+safeTail [] = []
+safeTail xs = tail xs
 
 stringDigits :: String -> [Int]
 stringDigits = map (read . (:""))
@@ -59,21 +75,37 @@ matrixAllDiags = (on (++) matrixDiags) <*> matrixRot90
 factorial :: Integral a => a -> a
 factorial x = product [2..x]
 
+split :: Eq a => a -> [a] -> [[a]]
+split x xs = case break (x ==) xs of
+                  (ys, [])     -> [ys]
+                  (ys, (_:zs)) -> ys : split x zs
+
+applyBetween :: (a -> a -> a) -> [a] -> [a]
+applyBetween f = concat . map g . safeTail . inits
+  where
+    g xs = map (f $ last xs) xs
+
+holes :: [a] -> [(a, [a])]
+holes []     = []
+holes (x:xs) = (x, xs) : (map . second) (x :) (holes xs)
+
+table :: (a -> b -> c) -> [a] -> [b] -> [[c]]
+table f ys xs = map (flip map xs . f) ys
+
 
 
 p001 :: Int
 p001 = sum [x | x <- [0..999], mod x 3 == 0 || mod x 5 == 0]
 
-p002 :: Int
-p002 = sum $ takeWhile (< 4000000) $ filter even fibs
+p002 :: Integer
+p002 = sum $ takeWhile (< 4000000) $ filter even $ fibs
 
 p003 :: Int
 p003 = maximum $ primeFactors 600851475143
 
 p004 :: Int
-p004 = maximum $ filter f $ liftA2 (*) xs xs
+p004 = maximum $ filter f $ applyBetween (*) [100..999]
   where
-    xs = [100..999]
     f = ((==) <*> reverse) . toDigits
 
 p005 :: Int
@@ -130,5 +162,23 @@ p020 = sum $ toDigits $ factorial 100
 p021 :: Int
 p021 = sum $ filter g [2..9999]
   where
-    f = sum . init . factors
+    f = sum . properDivisors
     g x = x /= f x && x == f (f x)
+
+p022 :: IO Int
+p022 = do
+    s <- readFile "data/p022.txt"
+    return $ h $ map g $ sort $ f s
+  where
+    f = map (init . tail) . split ','
+    g = sum . map ((subtract $ ord 'A' - 1) . ord)
+    h = sum . zipWith (*) [1..]
+
+p024 :: Int
+p024 = fromDigits $ (!! 999999) $ sort $ permutations [0..9]
+
+p025 :: Int
+p025 = fromJust $ findIndex (10^999 <=) fibs
+
+p029 :: Int
+p029 = length $ nub $ join (liftA2 (^)) [2..100]
